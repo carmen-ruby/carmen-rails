f2f106790ec6cb115b90021a4806bf2785f4e7d6
Jim Benton <jim@autonomousmachine.com>
2012-04-11 19:18:27 -0500

    Add country_select_tag and subregion_select_tag.

--- lib/carmen/rails/action_view/form_helper.rb
+++ lib/carmen/rails/action_view/form_helper.rb
@@ -2,25 +2,16 @@ module ActionView
   module Helpers
     module FormOptionsHelper
 
-      # Return select and option tags for the given object and method.
+      # Return select and subregion option tags for the given object and method.
       #
       # Uses region_options_or_select to generate the list of option tags.
       def subregion_select(object, method, parent_region_or_code, options={}, html_options={})
-        parent_region = case parent_region_or_code
-        when String
-          Carmen::Country.coded(parent_region_or_code)
-        when Array
-          parent_region_or_code.inject(Carmen::World.instance) { |parent, next_code|
-            parent.subregions.coded(next_code)
-          }
-        else
-          parent_region_or_code
-        end
+        parent_region = determine_parent(parent_region_or_code)
         tag = InstanceTag.new(object, method, self, options.delete(:object))
         tag.to_region_select_tag(parent_region, options, html_options)
       end
 
-      # Return select and option tags for the given object and method.
+      # Return select and country option tags for the given object and method.
       #
       # Uses region_options_or_select to generate the list of option tags.
       def country_select(object, method, options={}, html_options = {})
@@ -44,6 +35,43 @@ module ActionView
         main_options = parent_region.subregions.map { |r| [r.name, r.code] }
         region_options += options_for_select(main_options, selected)
         region_options.html_safe
+      end
+
+      # Return select tag with the name provided containing country option
+      # tags.
+      #
+      # Uses region_options_or_select to generate the list of option tags.
+      def country_select_tag(name, value, options={})
+        subregion_select_tag(name, value, Carmen::World.instance, options)
+      end
+      #
+      # Return select tag with the name provided containing subregion option
+      # tags.
+      #
+      # Uses region_options_or_select to generate the list of option tags.
+      def subregion_select_tag(name, value, parent_region_or_code, options = {})
+        options.stringify_keys!
+        parent_region = determine_parent(parent_region_or_code)
+        priority_regions = options.delete(:priority) || []
+        opts = region_options_for_select(parent_region, value, priority_regions)
+        html_options = {"name" => name,
+                        "id" => sanitize_to_id(name)}.update(options.stringify_keys)
+        content_tag(:select, opts, html_options)
+      end
+
+      private
+
+      def determine_parent(parent_region_or_code)
+        case parent_region_or_code
+        when String
+          Carmen::Country.coded(parent_region_or_code)
+        when Array
+          parent_region_or_code.inject(Carmen::World.instance) { |parent, next_code|
+            parent.subregions.coded(next_code)
+          }
+        else
+          parent_region_or_code
+        end
       end
     end
 
--- spec/carmen/action_view/helpers/form_helper_spec.rb
+++ spec/carmen/action_view/helpers/form_helper_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 class CarmenViewHelperTest < MiniTest::Unit::TestCase
   include ActionView::Helpers::FormOptionsHelper
+  include ActionView::Helpers::FormTagHelper
   include ActionDispatch::Assertions::SelectorAssertions
 
   def setup
@@ -29,6 +30,24 @@ class CarmenViewHelperTest < MiniTest::Unit::TestCase
   def test_country_selected_value
     @object.country_code = 'OC'
     @html = country_select(@object, :country_code)
+    assert_select('option[selected="selected"][value="OC"]')
+  end
+
+  def test_basic_country_select_tag
+    html = country_select_tag('attribute_name', nil)
+    expected = <<-HTML
+      <select id="attribute_name" name="attribute_name">
+        <option value="OC">Oceania</option>
+        <option value="EU">Eurasia</option>
+        <option value="ES">Eastasia</option>
+      </select>
+    HTML
+
+    assert_equal_markup(expected, html)
+  end
+
+  def test_country_tag_selected_value
+    @html = country_select_tag(:country_code, 'OC')
     assert_select('option[selected="selected"][value="OC"]')
   end
 
@@ -85,6 +104,17 @@ class CarmenViewHelperTest < MiniTest::Unit::TestCase
     @html = subregion_select(@object, :subregion_code, oceania)
 
     assert_select('option[selected="selected"][value="AO"]')
+  end
+
+  def test_basic_subregion_select
+    oceania = Carmen::Country.coded('OC')
+    expected = <<-HTML
+      <select id="subregion_code" name="subregion_code"><option value="AO">Airstrip One</option></select>
+    HTML
+
+    html = subregion_select_tag(:subregion_code, nil, oceania)
+
+    assert_equal_markup(expected, html)
   end
 
   def method_missing(method, *args)
