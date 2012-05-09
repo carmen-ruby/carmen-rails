34e0b5d003531693f96ce61060a7014736a53178
Jim Benton <jim@autonomousmachine.com>
2012-05-08 21:58:44 -0500

    Use Rail's I18n backend.

--- lib/carmen-rails.rb
+++ lib/carmen-rails.rb
@@ -3,8 +3,16 @@ require 'carmen/rails/version'
 
 module Carmen
   module Rails
-    #class Railtie < ::Rails::Railtie
-      # perform rails setup
-    #end
+    class Railtie < ::Rails::Railtie
+      # Add Carmen's locale paths to the Rails backend
+      paths = Carmen.i18n_backend.locale_paths.map { |path|
+        Dir[path + '**/*.yml']
+      }.flatten.compact
+      Carmen.i18n_backend = ::I18n
+      config.i18n.load_path += paths
+
+      # Enable fallbacks so that missing translations use the default locale
+      config.i18n.fallbacks = true
+    end
   end
 end
